59e0dd2797548f9802e59b05baeef51ed5679407
artembutbaev <avb49@bath.ac.uk>
2019-02-11 14:04:59 +0000

    PA-8: made a new activity

---
+++ app/src/main/java/uk/ac/bath/petmatch/SetUserCapabilitiesActivity.java
@@ -0,0 +1,13 @@
+package uk.ac.bath.petmatch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SetUserCapabilitiesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_user_capabilities);
+    }
+}
