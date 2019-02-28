a530e93ea66304e7df3a7cd82ae864cb7e6d0bb1
artembutbaev <avb49@bath.ac.uk>
2019-02-28 22:18:31 +0000

    added ShelterProfileActivity and Intent for opening it through the hamburger button

---
+++ app/src/main/java/uk/ac/bath/petmatch/ShelterProfileActivity.java
@@ -0,0 +1,17 @@
+package uk.ac.bath.petmatch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.Objects;
+
+public class ShelterProfileActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shelter_profile);
+
+        Objects.requireNonNull(getSupportActionBar()).setTitle("Shelter Profile");
+    }
+}
