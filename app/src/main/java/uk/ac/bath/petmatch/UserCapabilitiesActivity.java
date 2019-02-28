527c361fd2bc94c764c450bda38c929d954eb8b4
artembutbaev <avb49@bath.ac.uk>
2019-02-28 20:41:21 +0000

    Created UserCapabilitiesFragment and added preferences to the layout of User Capabilities. Deleted old strings used for the previous SettingsActivity.

--- app/src/main/java/uk/ac/bath/petmatch/UserCapabilitiesActivity.java
+++ app/src/main/java/uk/ac/bath/petmatch/UserCapabilitiesActivity.java
@@ -1,7 +1,10 @@
 package uk.ac.bath.petmatch;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.util.Objects;
+
+import uk.ac.bath.petmatch.Utils.UserCapabilitiesFragment;
 
 public class UserCapabilitiesActivity extends BaseActivity {
 
@@ -9,5 +12,16 @@ public class UserCapabilitiesActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_capabilities);
+
+        Objects.requireNonNull(getSupportActionBar()).setTitle("My Capabilities");
+
+        if(findViewById(R.id.fragment_container) != null) {
+
+            if(savedInstanceState != null ) {
+
+                return;
+            }
+            getFragmentManager().beginTransaction().add(R.id.fragment_container, new UserCapabilitiesFragment()).commit();
+        }
     }
 }
