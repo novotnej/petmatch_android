ca0843ba6375f3bf4ce13141dc3d36c0ef57be2b
Jabzilla <jab218@bath.ac.uk>
2019-03-01 09:34:22 +0000

    Only doing this so shit doesn't get overwritten

---
+++ app/src/main/java/uk/ac/bath/petmatch/PetAddActivity.java
@@ -0,0 +1,70 @@
+package uk.ac.bath.petmatch;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import uk.ac.bath.petmatch.Database.DbHelper;
+import uk.ac.bath.petmatch.Database.Pet;
+import uk.ac.bath.petmatch.Database.PetBreed;
+import uk.ac.bath.petmatch.Database.PetBreedDao;
+import uk.ac.bath.petmatch.Database.Shelter;
+
+public class PetAddActivity extends BaseActivity {
+
+    int RESULT_LOAD_IMAGE = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pet_add);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final EditText nameField = (EditText) findViewById(R.id.nameText);
+                String name = nameField.getText().toString();
+
+                final EditText emailField = (EditText) findViewById(R.id.descText);
+                String email = emailField.getText().toString();
+
+                Shelter whoCares = new Shelter("Dummy shelter", "This is the best shelter ever", "#3456780", "Radiator Springs, BA1 1SU", "51.377954, -2.356484", "spam@tobedeleted.com");
+
+                PetBreed dummy = new PetBreed("dummy", PetBreed.TYPE_DOG, false, false, false, false, false);
+                Pet newPet = new Pet(name, email, whoCares, dummy);
+
+
+
+
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        Button buttonLoadImage = (Button) findViewById(R.id.imButton);
+        buttonLoadImage.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent i = new Intent(
+                        Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+                startActivityForResult(i, RESULT_LOAD_IMAGE);
+            }
+
+
+        });
+    }
+
+
+}
