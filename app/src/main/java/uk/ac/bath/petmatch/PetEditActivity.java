2507a11d886f3f0951e90e81620f6d7874feb943
Jabzilla <jab218@bath.ac.uk>
2019-03-14 14:46:01 +0000

    Pet Edit now shows images and allows you to edit the breed

--- app/src/main/java/uk/ac/bath/petmatch/PetEditActivity.java
+++ app/src/main/java/uk/ac/bath/petmatch/PetEditActivity.java
@@ -6,15 +6,23 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import uk.ac.bath.petmatch.Database.Pet;
+import uk.ac.bath.petmatch.Database.PetBreed;
 import uk.ac.bath.petmatch.Database.PetDao;
 
 public class PetEditActivity extends BaseActivity {
+
+    PetBreed filterPetBreed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +39,59 @@ public class PetEditActivity extends BaseActivity {
 
         final EditText editDesc = (EditText)findViewById(R.id.petDesc);
         editDesc.setText(petClicked.getDescription(), TextView.BufferType.EDITABLE);
+
+        ImageView petImage = (ImageView)findViewById(R.id.petImage);
+        String id = petClicked.getImage();
+        int resID;
+        try {
+            resID = (int) R.mipmap.class.getField(id).get(null);
+        } catch (Exception e) {
+            Log.e("PetGridAdapter", e.getMessage());
+            resID = R.mipmap.dumbledore;
+        }
+        petImage.setImageResource(resID);
+        //petImageView.setImageResource(R.mipmap.dumbledore);
+
+        Spinner spinner = (Spinner) findViewById(R.id.breeds_spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+
+        //Get list of breeds in a given breed type
+        String[] breeds = getHelper().petBreeds.getArrayForType(PetBreed.TYPE_DOG);
+        final String[] spinnerBreeds = new String[breeds.length +1];
+        for (int i = 0; i < breeds.length; i++) {
+            spinnerBreeds[i] = breeds[i];
+        }
+        spinnerBreeds[breeds.length] = " --- ALL ---"; //add an "empty" choice option
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, spinnerBreeds);
+        spinner.setAdapter(arrayAdapter);
+
+
+        //Validate that the currently chosen filter value is a valid breed and is in the chosen breed type
+        if (filterPetBreed != null) {
+            int spinnerPosition = arrayAdapter.getPosition(filterPetBreed.getTitle());
+            if (spinnerPosition == -1) { //if selected breed not in the options, choose "empty" value
+                spinner.setSelection(spinnerBreeds.length - 1);
+            } else {
+                spinner.setSelection(spinnerPosition);
+            }
+        } else {
+            spinner.setSelection(spinnerBreeds.length - 1); //if no breed selected, choose "empty" valu
+        }
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectedBreedTitle = spinnerBreeds[position];
+                //spinner contains titles of pet breeds. Find breed by title and use the PetBreed model in filter
+                filterPetBreed = getHelper().petBreeds.loadByTitle(selectedBreedTitle);
+                petClicked.setBreed(filterPetBreed);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
 
         final Button button = findViewById(R.id.saveButton);
         button.setOnClickListener(new View.OnClickListener() {
