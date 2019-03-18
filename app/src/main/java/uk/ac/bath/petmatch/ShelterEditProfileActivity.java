36f43bdfec1b7ab8d5e8c7f4c80e9a62c1942d29
artembutbaev <avb49@bath.ac.uk>
2019-03-18 21:35:37 +0000

    changed name from ShelterProfileActivity to ShelterEditProfileActivity

--- app/src/main/java/uk/ac/bath/petmatch/ShelterEditProfileActivity.java
+++ app/src/main/java/uk/ac/bath/petmatch/ShelterEditProfileActivity.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 
 import java.util.Objects;
 
-public class ShelterProfileActivity extends BaseActivity {
+public class ShelterEditProfileActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
