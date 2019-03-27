d9e515cba955040d998ef153f26b75b1aa4a8d9c
artembutbaev <avb49@bath.ac.uk>
2019-03-27 12:03:29 +0000

    added buttons and changed layout of Welcome Screen

--- app/src/main/java/uk/ac/bath/petmatch/WelcomeScreenActivity.java
+++ app/src/main/java/uk/ac/bath/petmatch/WelcomeScreenActivity.java
@@ -13,17 +13,5 @@ public class WelcomeScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
-
 }
