e9e40c48dc3abaa23de1150464826d88aa4b6e7f
artembutbaev <avb49@bath.ac.uk>
2019-03-29 10:34:39 +0000

    welcome activity is now the starting activity and buttons work

--- app/src/main/java/uk/ac/bath/petmatch/WelcomeScreenActivity.java
+++ app/src/main/java/uk/ac/bath/petmatch/WelcomeScreenActivity.java
@@ -1,11 +1,14 @@
 package uk.ac.bath.petmatch;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class WelcomeScreenActivity extends AppCompatActivity {
 
@@ -13,5 +16,29 @@ public class WelcomeScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
+
+
+        Button loginButton = (Button) findViewById(R.id.welcome_login_button);
+        Button justBrowseButton = (Button) findViewById(R.id.welcome_browse_button);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent loginActivity = new Intent(getApplicationContext(),
+                        LoginActivity.class);
+                startActivity(loginActivity);
+            }
+        });
+
+        justBrowseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent mainActivity = new Intent(getApplicationContext(),
+                        MainActivity.class);
+                startActivity(mainActivity);
+            }
+        });
     }
 }
