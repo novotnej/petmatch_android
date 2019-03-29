88c3f03676a512060ff2e1e2d291e7c807b3fe05
Jan Novotny <novotny@profisites.it>
2019-03-29 10:35:09 +0000

    resolve conflicts

--- app/src/main/java/uk/ac/bath/petmatch/ShelterEditProfileActivity.java
+++ app/src/main/java/uk/ac/bath/petmatch/ShelterEditProfileActivity.java
@@ -3,24 +3,12 @@ package uk.ac.bath.petmatch;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.location.Address;
-import android.location.Geocoder;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import com.google.android.gms.maps.model.LatLng;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
 
 import uk.ac.bath.petmatch.Database.Shelter;
@@ -32,7 +20,7 @@ import uk.ac.bath.petmatch.Database.UserPropertiesDao;
 public class ShelterEditProfileActivity extends BaseActivity implements View.OnClickListener {
 
     private EditText shelterIntroduction;
-    private AutoCompleteTextView shelterAddress;
+    private EditText shelterAddress;
     private EditText shelterEmail;
     private EditText shelterCharityNumber;
     private EditText shelterPhoneNumber;
@@ -73,7 +61,7 @@ public class ShelterEditProfileActivity extends BaseActivity implements View.OnC
 
         TextView shelterTitle = (TextView) findViewById(R.id.shelter_profile_title_edit);
         shelterIntroduction = (EditText) findViewById(R.id.shelter_introduction_edit);
-        shelterAddress = (AutoCompleteTextView) findViewById(R.id.shelter_address_edit);
+        shelterAddress = (EditText) findViewById(R.id.shelter_address_edit);
         shelterEmail = (EditText) findViewById(R.id.shelter_email_edit);
         shelterCharityNumber = (EditText) findViewById(R.id.shelter_charity_no_edit);
         shelterPhoneNumber = (EditText) findViewById(R.id.shelter_phone_edit);
@@ -143,80 +131,6 @@ public class ShelterEditProfileActivity extends BaseActivity implements View.OnC
         });
         permissionError.create();
         permissionError.show();
-    }
-
-    ArrayList<String> placesList;
-    ArrayAdapter<String> placesListAdatper;
-    String placesURL;
-    String PLACES_BASE_URL = "https://maps.googleapis.com/maps/api/place/autocomplete/json?";
-
-    protected void setUpAutocomplete() {
-        shelterAddress.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                placesList = new ArrayList<String>();
-                updateList(s.toString());
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-            }
-        });
-    }
-
-    private void updateList(String place) {
-
-        String parameter = "input=" + place + "&types=geocode&sensor=true&key=" + PLACES_API_KEY;
-
-        placesURL = PLACES_BASE_URL + parameter;
-
-        updateListsList(placesURL);
-
-    }
-
-    public void updatePlacesList(ArrayList<String> list) {
-        placesList.clear();
-
-        for (int i=0; i<list.size(); i++) {
-            placesList.add(i, list.get(i).toString());
-        }
-
-        placesListAdatper = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, placesList) {
-            @Override
-            public View getView(int position, View convertView, ViewGroup parent)
-            {
-                View view = super.getView(position, convertView, parent);
-                android.widget.TextView text = (android.widget.TextView) view.findViewById(android.R.id.text1);
-                return view;
-            }
-        };
-
-        shelterAddress.setAdapter(placesListAdatper);
-        placesListAdatper.notifyDataSetChanged();
-    }
-
-    public void getAddressLatLong(String addressString) {
-        Geocoder coder = new Geocoder(getBaseActivity());
-        List<Address> address;
-        LatLng latLng = null;
-
-        try {
-            address = coder.getFromLocationName(addressString, 5);
-            if (address == null) {
-
-            } else {
-                Address location = address.get(0);
-                latLng = new LatLng(location.getLatitude(), location.getLongitude());
-            }
-        } catch (java.io.IOException ex) {
-            ex.printStackTrace();
-        }
     }
 }
 
